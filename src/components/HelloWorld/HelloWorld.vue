b66f0b44fb804b74ca3cd1b5ff7913e844d30c9b
pmedek <petr.medek@inventi.cz>
2021-03-29 16:42:11 +0200

    Hello world example

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: `Hello world!`,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+p {
+  color: blue;
+}
+</style>
